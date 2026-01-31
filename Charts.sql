24b3e12f8f63f6c820ddf0a7700969443054ecbc
Aishwarya Sah <146162125+Aishwaryagithubit@users.noreply.github.com>
2026-02-01 00:32:52 +0545

    charts added

---
+++ Charts.sql
@@ -0,0 +1,33 @@
+-- Salary Growth Over Time
+SELECT 
+    TO_CHAR(hiredate, 'YYYY') AS year,
+    SUM(sal) AS total_salary
+FROM emp
+GROUP BY TO_CHAR(hiredate, 'YYYY')
+ORDER BY year;
+/
+
+--Employee Job Role
+SELECT 
+    job,
+    COUNT(*) AS employee_count
+FROM emp
+GROUP BY job;
+
+--Average Salary by Department
+SELECT 
+    d.dname AS department_name,
+    ROUND(AVG(e.sal), 2) AS avg_salary
+FROM emp e
+JOIN dept d
+ON e.deptno = d.deptno
+GROUP BY d.dname
+ORDER BY avg_salary DESC;
+
+--Hiring Over Time
+SELECT 
+    TO_CHAR(hiredate, 'YYYY') AS hire_year,
+    COUNT(*) AS employees_hired
+FROM emp
+GROUP BY TO_CHAR(hiredate, 'YYYY')
+ORDER BY hire_year;
